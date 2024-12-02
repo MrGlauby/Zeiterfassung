bc03f25a3cc761a03d06f7ec5738a300912f7d8d
Thomas <thomas@air-von-thomas.speedport.ip>
2024-12-02 10:06:46 +0100

    Update Main

--- libs/zeiterfassung/user-list-data-access/src/lib/user-list.service.ts
+++ libs/zeiterfassung/user-list-data-access/src/lib/user-list.service.ts
@@ -1,30 +1,25 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { User, UserCreate } from './user.interface';
 
-@Injectable({providedIn: "root"})
-export class ZeiterfassungUserListService{
-
-// Private =  # zugriffsmodifiaktor accesModifire
-  #data: User[] =[];
+@Injectable({ providedIn: 'root' })
+export class ZeiterfassungUserListService {
+  // Private =  # zugriffsmodifiaktor accesModifire
+  #data: User[] = [];
   #counter = 0;
 
   // property name: type
-  public create(user:UserCreate):void {
-    this.#data.push({...user,id:this.#counter++});
-  };
+  public create(user: UserCreate): void {
+    this.#data.push({ ...user, id: this.#counter++ });
+  }
 
-  public getAll(): User[]{
+  public getAll(): User[] {
     return this.#data;
-  };
-
+  }
 }
-
-
 
 // #data: Private Property, ein Array vom Typ User[], um die Benutzer zu speichern.
 
 // #counter: Private Property, initialisiert mit 0, dient zur Vergabe eindeutiger IDs.
-
 
 // create(user: UserCreate): void:
 // Nimmt einen Parameter vom Typ UserCreate (definiert in user.interface.ts).
@@ -36,7 +31,6 @@ export class ZeiterfassungUserListService{
 // {...user} kopiert die Felder von user (Spread-Syntax).
 // id wird dynamisch mit dem aktuellen Wert von #counter hinzugefügt, der dann um 1 hochgezählt wird.
 // getAll(): Gibt das gesamte #data-Array als User[] zurück.
-
 
 //Fazit:
 // Spread: Verteilt die Werte (Kopieren/Erweitern). Der Spread-Operator wird verwendet, um die Inhalte eines Arrays oder Objekts zu "entpacken" und zu verteilen.
