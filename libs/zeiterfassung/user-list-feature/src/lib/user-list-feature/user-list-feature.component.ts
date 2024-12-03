76f0a9e88b28535b4f9a779bde9d6fd9506edf93
Thomas <thomas@MacBook-Air-von-Thomas.local>
2024-12-03 10:49:43 +0100

    Table Update

--- libs/zeiterfassung/user-list-feature/src/lib/user-list-feature/user-list-feature.component.ts
+++ libs/zeiterfassung/user-list-feature/src/lib/user-list-feature/user-list-feature.component.ts
@@ -1,21 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ZeiterfassungUserListService } from '@zeiterfassung/user-list-data-access';
+import { User } from 'libs/zeiterfassung/user-list-data-access/src/lib/user.interface';
+import { MatTableModule } from '@angular/material/table'; // Importiere das MatTableModule
+import { CdkTableModule } from '@angular/cdk/table';
 
 @Component({
   selector: 'lib-user-list-feature',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './user-list-feature.component.html',
   styleUrl: './user-list-feature.component.css',
+  imports: [CommonModule, MatTableModule, CdkTableModule],
 })
 export class UserListFeatureComponent {
-
   userListService = inject(ZeiterfassungUserListService);
 
-  public createUser():void{
-    this.userListService.create({name:'Max', lastname:'Mustermann', group:'employee' });
-    console.log('jo geht')
-  };
+  public users: User[] = [];
+  public displayedColumns: string[] = ['name', 'group', 'lastname']; // Spaltennamen
 
+  public createUser(): void {
+    this.userListService.create({
+      name: 'Max',
+      lastname: 'Mustermann',
+      group: 'employee',
+    });
+    this.users = this.userListService.getAll();
+    console.log('User hinzugefügt:', this.users);
+  }
 }
